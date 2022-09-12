bdc43333f1703f4bef56bdf0675c91c3e4aa885d
kimsubeen <papajj06@naver.com>
2022-09-12 22:50:54 +0900

    [ch02] layout : button, text

--- app/src/main/java/com/sopt/ob/houseparty/MainActivity.kt
+++ app/src/main/java/com/sopt/ob/houseparty/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.sopt.ob.houseparty
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.sopt.ob.houseparty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        clickEvent()
+    }
+
+    private fun addNickname(view: View) {
+        binding.tvName.text = binding.etName.text
+        binding.etName.visibility = View.GONE
+        view.visibility = View.GONE
+        binding.tvName.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun clickEvent() {
+        binding.btnDone.setOnClickListener {
+            addNickname(it)
+        }
     }
 }
