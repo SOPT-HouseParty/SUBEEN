d0d43395fe92c91bbba6169ce0ccdb3ada957bcb
kimsubeen <papajj06@naver.com>
2022-09-13 00:07:13 +0900

    [ch02] layout : DataBinding

--- app/src/main/java/com/sopt/ob/houseparty/MainActivity.kt
+++ app/src/main/java/com/sopt/ob/houseparty/MainActivity.kt
@@ -5,24 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
 import com.sopt.ob.houseparty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName = MyName("KIM SU BEEN")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+        
         clickEvent()
     }
 
     private fun addNickname(view: View) {
-        binding.tvName.text = binding.etName.text
-        binding.etName.visibility = View.GONE
-        view.visibility = View.GONE
-        binding.tvName.visibility = View.VISIBLE
+        binding.apply {
 
+            myName?.nickname = etName.text.toString()
+
+            etName.visibility = View.GONE
+            view.visibility = View.GONE
+            tvName.visibility = View.VISIBLE
+        }
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
