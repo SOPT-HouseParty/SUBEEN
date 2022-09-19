8ee373198af7972d4ca7f1accef01551a5563670
kimsubeen <papajj06@naver.com>
2022-09-19 17:56:23 +0900

    [FEAT] TitleFragment 구현

--- app/src/main/java/com/sopt/ob/houseparty/TitleFragment.kt
+++ app/src/main/java/com/sopt/ob/houseparty/TitleFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.sopt.ob.houseparty.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -17,6 +19,15 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        //findNavController 함수
+        binding.btnStart.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+        //익명함수로 가능
+       /* binding.btnStart.setOnClickListener {
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        }*/
         return binding.root
     }
 
